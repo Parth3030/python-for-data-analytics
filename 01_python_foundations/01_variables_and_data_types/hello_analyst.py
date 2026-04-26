19f3f652e2d5496dfea1a8487913bf6b1cdae912
parth <109512441+Parth3030@users.noreply.github.com>
2026-04-26 15:38:57 +0530

    Delete 01_python_foundations/01_variables_and_data_types directory

--- 01_python_foundations/01_variables_and_data_types/hello_analyst.py
+++
@@ -1,7 +0,0 @@
-# 1. Setting up my profile variables
-name = "Parth"
-city = "Ahmedabad"
-tool = "Python"
-
-# 2. Printing the introduction using an f-string
-print(f"Hello! I am {name}, a data analyst from {city} learning {tool}.")
--- 01_python_foundations/01_variables_and_data_types/jupyter_vs_.py_file.py
+++
@@ -1,7 +0,0 @@
-# 1. Defining the tools
-exploration_tool = "Jupyter Notebook"
-production_tool = ".py file"
-
-# 2. Explaining their use cases
-print(f"Exploration tool: {exploration_tool} | Best for: Testing code, visualizing data, quick experiments and cleaning data.")
-print(f"Production tool: {production_tool}  | Best for: Final clean code, automation, and running scripts on a server.")
